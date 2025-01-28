f0048635ed7c59c131dc2b261ce4a0f9a58e2856
Mike Glenn <mjglenn+lf@gmail.com>
2025-01-28 20:03:42 +0000

    combined vm insert/update into upsert on conflict

--- server/virtual-machines.ts
+++ server/virtual-machines.ts
@@ -25,63 +25,38 @@ async function dbWriteVMs(
   virtualMachines: VirtualMachine[]
 ): Promise<void> {
   for (const vm of virtualMachines) {
-    const sqlCheckIId = `
-      select * from "virtualMachines"
-       where "instanceId" = $1;
+    const sql = `
+      insert into "virtualMachines" ("accountId", "name", "instanceId",
+      "region", "vpcId", "subnetId", "state", "type", "os", "privateIp",
+      "publicIp", "tags", "launchTime")
+      values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+      on conflict ("instanceId")
+      do update set "name"              = $2,
+                    "state"             = $7,
+                    "type"              = $8,
+                    "os"                = $9,
+                    "privateIp"         = $10,
+                    "publicIp"          = $11,
+                    "tags"              = $12,
+                    "launchTime"        = $13
+      returning *;
     `;
-    const paramsCheckIId = [vm.instanceId];
-    const resultCheckIId = await db.query(sqlCheckIId, paramsCheckIId);
-    if (resultCheckIId.rows) {
-      const sqlUpdateVM = `
-        update "virtualMachines"
-           set "name"              = $2,
-               "state"             = $3,
-               "type"              = $4,
-               "os"                = $5,
-               "privateIp"         = $6,
-               "publicIp"          = $7,
-               "tags"              = $8,
-               "launchTime"        = $9
-         where "instanceId" = $1
-         returning *;
-      `;
-      const paramsUpdateVM = [
-        vm.instanceId,
-        vm.name,
-        vm.state,
-        vm.type,
-        vm.os,
-        vm.privateIp,
-        vm.publicIp,
-        vm.tags,
-        vm.launchTime,
-      ];
-      await db.query(sqlUpdateVM, paramsUpdateVM);
-    } else {
-      const sqlCreateVM = `
-          insert into "virtualMachines" ("accountId", "name", "instanceId",
-          "region", "vpcId", "subnetId", "state", "type", "os", "privateIp",
-          "publicIp", "tags", "launchTime")
-          values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
-          returning *;
-        `;
-      const paramsCreateVM = [
-        accountId,
-        vm.name,
-        vm.instanceId,
-        vm.region,
-        vm.vpcId,
-        vm.subnetId,
-        vm.state,
-        vm.type,
-        vm.os,
-        vm.privateIp,
-        vm.publicIp,
-        vm.tags,
-        vm.launchTime,
-      ];
-      await db.query(sqlCreateVM, paramsCreateVM);
-    }
+    const params = [
+      accountId,
+      vm.name,
+      vm.instanceId,
+      vm.region,
+      vm.vpcId,
+      vm.subnetId,
+      vm.state,
+      vm.type,
+      vm.os,
+      vm.privateIp,
+      vm.publicIp,
+      vm.tags,
+      vm.launchTime,
+    ];
+    await db.query(sql, params);
   }
 }
 
